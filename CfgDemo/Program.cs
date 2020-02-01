733d90a1ba6ac857eb08a1f6c4dbc3075e0cf339
codewitch honey crisis <gaztoast@gmail.com>
2020-01-31 18:03:31 -0800

    improved demo

--- CfgDemo/Program.cs
+++ CfgDemo/Program.cs
@@ -1,6 +1,6 @@
 ﻿using C;
 using System;
-
+using System.Collections.Generic;
 
 namespace CfgDemo
 {
@@ -16,6 +16,8 @@ namespace CfgDemo
 			var cfg = CfgDocument.ReadFrom(args[0]);
 			// not-necessary but faster access since we're not modifying:
 			cfg.RebuildCache();
+			Console.WriteLine("See: http://hackingoff.com/compilers/ll-1-parser-generator");
+			Console.WriteLine();
 			Console.WriteLine("CFG has {0} rules composed of {1} non-terminals and {2} terminals for a total of {3} symbols" ,cfg.Rules.Count, cfg.FillNonTerminals().Count, cfg.FillTerminals().Count, cfg.FillSymbols().Count);
 			Console.WriteLine();
 			Console.Write("Terminals:");
@@ -62,6 +64,60 @@ namespace CfgDemo
 				Console.WriteLine();
 
 			}
+			Console.WriteLine("Building simple parse table");
+
+			var parseTable = new Dictionary<string, Dictionary<string, CfgRule>>();
+			foreach (var nt in cfg.FillNonTerminals())
+			{
+				var d = new Dictionary<string, CfgRule>();
+				parseTable.Add(nt, d);
+				foreach(var p in predict[nt])
+				{
+					if(null!=p.Symbol)
+					{
+						CfgRule or;
+						if(d.TryGetValue(p.Symbol,out or))
+						{
+							Console.Error.WriteLine("First-first conflict between " + p.Rule + " and " + or);
+						} else
+							d.Add(p.Symbol, p.Rule);
+					} else
+					{
+						foreach(var f in follows[nt])
+						{
+							CfgRule or;
+							if (d.TryGetValue(f, out or))
+							{
+								Console.Error.WriteLine("First-follows conflict between " + p.Rule + " and " + or);
+							}
+							else
+								d.Add(f, p.Rule);
+						}
+					}
+				}
+			}
+			Console.WriteLine("Building simple lexer");
+			var fas = new FA[] 
+			{
+				FA.Literal("+","add"),
+				FA.Literal("*","mul"),
+				FA.Literal("(","lparen"),
+				FA.Literal(")","rparen"),
+				FA.Repeat(FA.Set("0123456789"), "int") 
+			};
+			var lexer = new FA();
+			for(var i = 0;i<fas.Length;i++)
+				lexer.EpsilonTransitions.Add(fas[i]);
+			Console.WriteLine();
+			var text = "(1+3)*2";
+			
+			Console.WriteLine("Creating tokenizer");
+			var tokenizer = new Tokenizer(lexer, text);
+			Console.WriteLine("Creating parser");
+			var parser = new Parser(parseTable, tokenizer, "Expr");
+			Console.WriteLine();
+			Console.WriteLine("Parsing " + text);
+			Console.WriteLine(parser.ParseSubtree());
 		}
 	}
 }
